37bb75f1666028bbbab4add8131055fbe53cd5fd
Kanwalpreet Singh <kanwalmachhiwara@gmail.com>
2024-02-20 12:33:20 -0500

    Kanwal.java comment added

---
+++ src/Kanwal/Kanwal.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Kanwal;
+
+/**
+ *
+ * @author kanwalsaini
+ * this java file is created by Kanwalpreet Singh on 20 feb 2024.
+ */
+public class Kanwal {
+    
+}
